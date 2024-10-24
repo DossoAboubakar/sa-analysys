289b5614dda6b506f28de6ac231bbf128ebbba68
Aboubakar <MinPAVR_AsNbFaeuxNzQ>
2024-10-24 12:27:34 +0000

    Projet initial

---
+++ src/main/java/tech/chillo/sa_analysys/Sa.java
@@ -0,0 +1,6 @@
+package tech.chillo.sa_analysys;
+
+public record Sa(int id ,
+                 String message , int status) {
+
+}
---
+++ src/main/java/tech/chillo/sa_analysys/SaController.java
@@ -0,0 +1,16 @@
+package tech.chillo.sa_analysys;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/sa", produces = MediaType.APPLICATION_JSON_VALUE)
+public class SaController {
+     public List<Object> search() {
+         return List.of(new Sa(1, "belle formation"  , 1));
+
+    }
+}
