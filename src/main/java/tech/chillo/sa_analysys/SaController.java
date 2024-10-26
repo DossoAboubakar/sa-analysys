6f9a326bf78cd8949dae462166db3abff7c7c652
Aboubakar <MinPAVR_AsNbFaeuxNzQ>
2024-10-26 02:52:16 +0000

    githubactions updated

--- src/main/java/tech/chillo/sa_analysys/SaController.java
+++ src/main/java/tech/chillo/sa_analysys/SaController.java
@@ -1,6 +1,7 @@
 package tech.chillo.sa_analysys;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/sa", produces = MediaType.APPLICATION_JSON_VALUE)
 public class SaController {
+    @GetMapping
      public List<Object> search() {
          return List.of(new Sa(1, "belle formation"  , 1));
 
